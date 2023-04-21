c2876d6a36b1ddce49b81fae151fc521f6c6014a
Sneh Pallav <snehpallav@Snehs-MacBook-Pro.local>
2023-04-21 23:44:02 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { numberArray } from './constants';
+import NumberTile from './NumberTile';
+import { useCallback, useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [numbers, setNumbers] = useState(numberArray);
+    const handleClick= useCallback((item)=>{
+      const index = numbers.findIndex((num)=>num === item);
+       const tempArr =  [...numbers.slice(0,index),...numbers.slice(index+1,numbers.length)]
+       setNumbers(tempArr);
+    },[numbers])
+
+    return (
+      <SafeAreaView>
+      <View style={styles.container}>
+        {numbers.map((number) => {
+          return (
+            <NumberTile handleClick={handleClick} number={number} key={number}/>
+          );
+        })}
+      </View>
+      </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flexDirection: "row",
+    width: "100%",
+    height: "100%",
+    flexWrap:'wrap',
+    marginTop: 20,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
 });
---
+++ NumberTile.js
@@ -0,0 +1,65 @@
+import React, { useMemo } from "react";
+import {
+    Pressable,
+    StyleSheet,
+    Text,
+    Animated,
+  } from "react-native";
+  
+const NumberTile = ({ handleClick, number }) => {
+    let animatedValue = new Animated.Value(0);
+    let currentValue = 0;
+    const generateRandomColor = () => {
+        return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    }
+    const color = useMemo(() => generateRandomColor(), []);
+
+    animatedValue.addListener(({ value }) => {
+    currentValue = value;
+    });
+    const flipToBack = () => {
+          Animated.timing(animatedValue, {
+            duration:400,
+              toValue: 180,
+              useNativeDriver: false,
+            }).start();
+      };
+
+      const handleItemClick = (number) => {
+        flipToBack();
+        setTimeout(()=>{
+            handleClick(number)
+        },400)
+      }
+
+      const setInterpolate = animatedValue.interpolate({
+        inputRange: [180, 360],
+        outputRange: ['180deg', '360deg'],
+      });
+    
+      const rotateYAnimatedStyle = {
+        transform: [{ rotateY: setInterpolate }],
+      };
+    return (
+        <Animated.View style={[styles.cardContainer, rotateYAnimatedStyle, {backgroundColor: color}]} >
+        <Pressable onPress={()=>handleItemClick(number)} style={styles.card}>
+            <Text>{number}</Text>
+        </Pressable>
+        </Animated.View>
+      );
+}
+
+const styles=StyleSheet.create({
+    cardContainer:{
+        height: 80,
+        width: 80,
+        margin:20,
+    },
+    card:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
+
+export default React.memo(NumberTile);
